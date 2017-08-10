76d4897c4f2cefa07792b6c03a9bce3d921eefbf
Luke Fabish <luke@lukefabish.com>
2017-08-11 09:41:28 +1000

    Update tests for AudioMute component.

--- test/AudioMute.spec.js
+++ test/AudioMute.spec.js
@@ -6,46 +6,47 @@ import AudioMute from '../app/AudioMute';
 
 describe('AudioMute', () => {
   const nullFn = () => {};
-  const audioOnImg = "img/speaker-vol.png";
-  const audioOffImg = "img/speaker-no-vol.png";
+  const expectedElement = 'button';
+  const audioOnImg = "url('img/speaker-vol.png')";
+  const audioOffImg = "url('img/speaker-no-vol.png')";
 
   const defaultWrapper = shallow(
     <AudioMute
-      muteOn={false}
+      muted={false}
       clickFn={nullFn}
     />
   );
 
   it('displays image', () => {
-    expect(defaultWrapper.find('img')).to.have.length(1);
+    expect(defaultWrapper.find(expectedElement)).to.have.length(1);
   });
 
-  it('displays "audio on" image with muteOn is false', () => {
-    let theImage = defaultWrapper.find('img');
-    expect(theImage.prop('src')).to.equal(audioOnImg);
+  it('displays "audio on" image with muted is false', () => {
+    let theImage = defaultWrapper.find(expectedElement);
+    expect(theImage.prop('style').backgroundImage).to.equal(audioOnImg);
   });
 
-  it('displays "audio off" image with muteOn is true', () => {
+  it('displays "audio off" image with muted is true', () => {
     const wrapper = shallow(
       <AudioMute
-        muteOn={true}
+        muted={true}
         clickFn={nullFn}
       />
     );
 
-    let theImage = wrapper.find('img');
-    expect(theImage.prop('src')).to.equal(audioOffImg);
+    let theImage = wrapper.find(expectedElement);
+    expect(theImage.prop('style').backgroundImage).to.equal(audioOffImg);
   });
 
   it('execute clickFn property function when clicked', () => {
     let clickFn_spy = sinon.spy();
     const wrapper = shallow(
       <AudioMute
-        muteOn={true}
+        muted={true}
         clickFn={clickFn_spy}
       />
     );
-    let theImage = wrapper.find('img');
+    let theImage = wrapper.find(expectedElement);
     theImage.simulate('click');
     expect(clickFn_spy.calledOnce).to.be.true;
   });
